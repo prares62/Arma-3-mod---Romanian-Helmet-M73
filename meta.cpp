0fc0ffe77c2b6aedba3ec79320724e2e50ae283a
prares62 <poprares62@gmail.com>
2026-05-07 15:17:54 +0300

    Added mod folder

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 2898673779;
+name = "Romanian Helmet M73";
+timestamp = 5249898274808459932;
---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name		 = "Romanian Helmet M73";								// Name of your mod
+logoSmall	 = "logo_small.paa";	// Display next to the item added by the mod
+logo		 = "logo.paa";			// Logo displayed in the main menu
+logoOver	 = "logo.paa";		// When the mouse is over, in the main menu
+tooltipOwned = "It's yours!";								// Tool tip displayed when the mouse is left over, in the main menu
+
+// Overview text, displayed from the extension menu
+overview = "It just adds a helmet.";
+hideName	= 0;	// Hide the extension name
+hidePicture	= 0;	// Hide the extension menu
